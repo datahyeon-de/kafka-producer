13d412e615155347c6adf433833fe6d511d349b6
datahyeon <hyeondata@gmail.com>
2025-08-13 14:27:46 +0900

    ch5.6-1 작성

--- producers/bicycle_producer.py
+++ producers/bicycle_producer.py
@@ -57,7 +57,7 @@ class BicycleProducer():
                 try:
                     self.producer.produce(
                         topic=self.topic,
-                        key=json.dumps({'STT_ID': item['STT_ID'], 'CRT': item['CRT']}, ensure_ascii=False),
+                        key=json.dumps({'STT_ID': item['STT_ID'], 'CRT_DTTM': item['CRT_DTTM']}, ensure_ascii=False),
                         value=json.dumps(item, ensure_ascii=False),
                         on_delivery=self.delivery_callback,
                     )
